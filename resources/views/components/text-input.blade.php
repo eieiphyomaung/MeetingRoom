fe1d4eeb66b0476e7e101b6562ec3d2b098b7897
Ei Ei Phyo Maung <eieiphyomaung92@gmail.com>
2026-01-14 06:37:18 +0630

    Update Function

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,8 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input {{ $disabled ? 'disabled' : '' }}
+    {!! $attributes->merge([
+        'class' =>
+            'border-white/50 bg-white/55 text-slate-900 placeholder:text-slate-500
+             focus:border-sky-400 focus:ring-sky-400/30 rounded-xl shadow-sm'
+    ]) !!} >
